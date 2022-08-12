d00ec5e346d1de084f409da131774d737f48e0e7
Mahesh <deshmukhmahesh009@gmail.com>
2022-08-12 09:39:21 +0530

    style: information

---
+++ src/components/information/Information.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const InformationContainer = styled.main`
+  background-color: #fff2f2;
+  min-height: 100vh;
+  text-align: center;
+  padding: 1rem 0;
+`;
